485bee7ee35f9d0e3a7685991e596046b7e2deaa
unknown <t-twu@MININT-O45UU34.fareast.corp.microsoft.com>
2013-09-16 11:48:36 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using LogReporter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Log.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string configFile = "config.xml";
+
+            if (args.Length > 0) configFile = args[0];
+
+            XmlDocument xd = new XmlDocument();
+            xd.Load(configFile);
+
+            Reporter report = new Reporter(xd.FirstChild);
+            Console.WriteLine("Report Started:" + DateTime.Now);
+            report.Start();
+            Console.WriteLine("Report Finished:" + DateTime.Now);
+
+        }
+    }
+}
